228d6e37ac34b1d1fa35ef34b2394cc35d259554
ArcherSore <17321322251@163.com>
2024-02-20 20:29:28 +0800

    习惯的方法bfs

--- AtCoder Beginner Contest 339/D_Synchronized_Players.cpp
+++ AtCoder Beginner Contest 339/D_Synchronized_Players.cpp
@@ -47,7 +47,7 @@ int main() {
     for (auto [x, y] : init) {
         q.push({x, y, 0});
     }
-
+    vis[init[0].first][init[0].second][init[1].first][init[1].second] = 1;
     while (!q.empty()) {
         int step = q.front().step;
         int x1 = q.front().x, y1 = q.front().y;
@@ -55,35 +55,32 @@ int main() {
         int x2 = q.front().x, y2 = q.front().y;
         q.pop();
 
-        if (vis[x1][y1][x2][y2]) {
-            continue;
-        }
-        vis[x1][y1][x2][y2] = 1;
         // cout << x1 + 1 << " " << y1 + 1 << " " << x2 + 1 << " " << y2 + 1 << '\n';
-
-        if (x1 == x2 && y1 == y2) {
-            cout << step << '\n';
-            return 0;
-        }
 
         for (int i = 0; i < 4; i++) {
             int a = x1 + dx[i], b = y1 + dy[i];
             int c = x2 + dx[i], d = y2 + dy[i];
-            
-            if (s[a][b] == '#' && s[c][d] == '#') {
+
+            if (s[a][b] == '#') {
+                a = x1, b = y1;
+            }
+            if (s[c][d] == '#') {
+                c = x2, d = y2;
+            }
+
+            if (s[a][b] == '#' && s[c][d] == '#' || vis[a][b][c][d]) {
                 continue;
             }
 
-            if (s[a][b] == '#') {
-                q.push({x1, y1, step + 1});
-            } else {
-                q.push({a, b, step + 1});
+            vis[a][b][c][d] = 1;
+            
+            if (a == c && b == d) {
+                cout << step + 1 << '\n';
+                return 0;
             }
-            if (s[c][d] == '#') {
-                q.push({x2, y2, step + 1});
-            } else {
-                q.push({c, d, step + 1});
-            }
+
+            q.push({a, b, step + 1});
+            q.push({c, d, step + 1});
         }
     }
 
